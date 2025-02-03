2fe567d21d4d281c03ea8862a7bf90025fd44d42
Emanuele Dinaro <ema.dinaro@gmail.com>
2025-02-03 12:52:49 +0100

    feat: add <li> with product list array

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
 import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [productList, setproductList] = useState([
+    "Pane",
+    "mozzarella",
+    "pomodoro",
+  ]);
 
-  return <></>;
+  return (
+    <>
+      <h1>Products List</h1>
+      <ul>
+        {productList.map((product, index) => {
+          return <li key={index}>{product}</li>;
+        })}
+      </ul>
+      <hr />
+      <form>
+        <input type="text" placeholder="Inserisci un nuovo prodotto" />
+        <input type="submit" />
+      </form>
+    </>
+  );
 }
 
 export default App;
