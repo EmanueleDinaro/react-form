5f13df072f0ed6d6c9a6c9bb6d468b6a2c188d57
Emanuele Dinaro <ema.dinaro@gmail.com>
2025-02-03 19:55:26 +0100

    feat: add onChange event

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,22 @@ function App() {
     "mozzarella",
     "pomodoro",
   ]);
+  const [newProduct, setNewProduct] = useState("")
+ 
+console.log(newProduct);
 
   return (
     <>
       <h1>Products List</h1>
       <ul>
         {productList.map((product, index) => {
-          return <li key={index}>{product}</li>;
+          return <li key={index}>{product}
+          <button>cancella</button></li>;
         })}
       </ul>
       <hr />
       <form>
-        <input type="text" placeholder="Inserisci un nuovo prodotto" />
+        <input type="text" onChange={event => {setNewProduct(event.target.value)}} placeholder="Inserisci un nuovo prodotto" />
         <input type="submit" />
       </form>
     </>
